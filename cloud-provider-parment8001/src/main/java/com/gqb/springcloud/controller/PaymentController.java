a9073150cbcb5c99e2e7b6f9bcac90d45fc02631
1720653171 <37207723+1720653171@users.noreply.github.com>
2020-10-15 16:34:08 +0800

    修改

--- cloud-provider-parment8001/src/main/java/com/gqb/springcloud/controller/PaymentController.java
+++ cloud-provider-parment8001/src/main/java/com/gqb/springcloud/controller/PaymentController.java
@@ -1,25 +1,40 @@
 package com.gqb.springcloud.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gqb.springcloud.entities.CommonResult;
+import com.gqb.springcloud.entities.Payment;
+import com.gqb.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 @RestController
+@Slf4j
 public class PaymentController {
 
-    @RequestMapping("/test")
-    public String Test(){
-        return "sucess";
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payment/insert")
+    public CommonResult insert(@RequestBody(required=false) Payment payment){
+        int result = paymentService.insert(payment);
+        log.info("*****插入结果："+result);
+        if(result>0){
+            return new CommonResult(200,"插入成功");
+        }else{
+            return new CommonResult(400,"插入失败");
+        }
     }
 
-    @RequestMapping("/test2")
-    public String Test2() {
+    @RequestMapping(value = "/payment/select/{id}")
+    public CommonResult selectById(@PathVariable("id") Long id){
 
-        return "2222";
+        Payment payment = paymentService.SelectById(id);
+        if(payment!=null){
+            return new CommonResult(200,"数据查询成功",payment);
+        }else {
+            return new CommonResult(404,"数据不存在",null);
+        }
     }
 
-    @RequestMapping("/test3")
-    public String Test3() {
-
-        return "2223";
-    }
 }
